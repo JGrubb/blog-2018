9551609748097c2dd657d5c96868fddc7756c4e1
John Grubb <grubb@johnnygrubb.com>
2018-03-27 21:09:44 -0400

    adding sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
   resources :posts, except: :show
-  get 'posts/:id-:slug', to: 'posts#show', as: :show_post, constraints: { id: /\d+/}
+  get 'posts/:id-:slug', to: 'posts#show', as: :show_post, constraints: {id: /\d+/}
   root 'posts#front'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
